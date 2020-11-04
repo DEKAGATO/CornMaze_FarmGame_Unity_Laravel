fdea49af40e5338be60a37101dde10a1a9b7af88
MobileWebLinja <67433114+MobileWebLinja@users.noreply.github.com>
2020-11-04 02:10:52 -0800

    version 1.0

---
+++ Corn Maze_game(Unity3d)/Assets/Scripts/UI/HomeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScene : MonoBehaviour
+{
+    public void CornMazeLogoClick()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void CitrouillevilleLogoClick()
+    {
+        Application.LoadLevel("Citrouilleville Info");
+    }
+}
